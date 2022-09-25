2789753ab47eb8e0019b4963bf9c5ec5bca0f889
Tomer Peleg <tomer.peleg@gmail.com>
2022-09-25 11:14:02 +0100

    Add mock data

---
+++ mocks/mock_fixer.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+/**
+ * This is file is modifying mock data generated using mockaroo.com to match the given spec
+ */
+
+const LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const randLetters = (n) =>
+  new Array(n)
+    .fill(0)
+    .map(() => LETTERS[Math.floor(Math.random() * LETTERS.length)])
+    .join("");
+
+const randNumbers = (n) =>
+  new Array(n)
+    .fill(0)
+    .map(() => Math.floor(Math.random() * 10))
+    .join("");
+
+const generateId = () => `${randLetters(3)}-${randNumbers(3)}`;
+
+const data = JSON.parse(
+  fs.readFileSync("trial_participants.json", { endoing: "utf8" })
+).map((participant) => ({ ...participant, id: generateId() }));
+
+fs.writeFileSync("trial_participants.json", JSON.stringify(data));
